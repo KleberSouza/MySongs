1922c11af4d267f83a268e3b6982061c5ad3d60b
KleberSouza <kleberjfsouza@gmail.com>
2021-09-28 19:30:31 -0300

    Adicionar arquivos de projeto.

---
+++ MySongs/Models/Album.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySongs.Models
+{
+    public class Album
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public int Ano { get; set; }
+
+    }
+}
---
+++ MySongs/Models/Musica.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySongs.Models
+{
+    public class Musica
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public TimeSpan Duracao { get; set; }
+
+        public DateTime DataLançamento { get; set; }
+    }
+}
